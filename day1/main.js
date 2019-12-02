ea99a9d27e2b5db100918d9d62b268c98c1120f9
James Royal <jhr.atx@gmail.com>
2019-12-01 21:04:38 -0600

    Day 1 part 2

--- day1/main.js
+++ day1/main.js
@@ -1,12 +1,24 @@
 const { readFileToArray } = require("../utils");
 
+const basicFuelCalc = mass => Math.floor(mass / 3) - 2;
+const sum = (prev, current) => prev + current;
+
+function extendedFuelCalc(mass) {
+  let fuelCount = basicFuelCalc(mass);
+  if (fuelCount <= 0) {
+    return 0;
+  }
+  return fuelCount + extendedFuelCalc(fuelCount);
+}
+
 async function main() {
   const inputs = await readFileToArray("day1/input.txt");
-  console.log(
-    inputs
-      .map(mass => Math.floor(mass / 3) - 2)
-      .reduce((prev, current) => prev + current)
-  );
+
+  // part 1
+  console.log(inputs.map(basicFuelCalc).reduce(sum));
+
+  //part 2
+  console.log(inputs.map(extendedFuelCalc).reduce(sum));
 }
 
 main();
